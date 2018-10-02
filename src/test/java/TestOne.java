68adcab7233a5fcdd563c531373744e934048a1d
UserApl-Sokolov <Vasiliy.Sokolov@aplana.com>
2018-10-03 01:46:08 +0300

    - Добавил чтение properties и сами properties по-умолчанию - Добавил родительский класс jUnit-тестов - Добавил пробный тест

---
+++ src/test/java/TestOne.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class TestOne extends BaseTest {
+
+    @Test
+    public void myFirstTest() throws InterruptedException {
+        driver.get(baseUrl);
+    }
+
+}
